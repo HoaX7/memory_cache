589c4dead4dbad33c5910aa0626c2de5f4fa23c9
HoaX7 <krhoax7@gmail.com>
2021-12-08 11:44:59 +0530

    build update

--- types/redis.ts
+++ types/redis.ts
@@ -2,9 +2,9 @@ export interface RedisClient {
     get: (key: string) => Promise<string | null>;
     set: (key: string, value: string) => Promise<string | null>;
     del: (key: string) => Promise<number>;
-    expire: (key: string, ttl: number) => Promise<boolean>;
-    flushAll: () => void;
-    keys: (pattern: string) => Promise<string[] | null>;
+    expire?: (key: string, ttl: number) => Promise<boolean>;
+    flushAll?: () => void;
+    keys?: (pattern: string) => Promise<string[] | null>;
 }
 export type Props = {
     password?: string;
